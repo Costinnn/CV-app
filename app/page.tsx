3d1e3fe5be10610e4a1c180b46b609014a343840
Costinnn <crauciuc.costin@yahoo.com>
2024-06-02 11:11:03 +0300

    How to create CV homepage section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Herosection from "@/components/pages/home/Herosection";
+import HowTo from "@/components/pages/home/HowTo";
 import WhatIsCv from "@/components/pages/home/WhatIsCv";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main>
       <Herosection />
       <WhatIsCv />
+      <HowTo />
     </main>
   );
 }
---
+++ components/pages/home/HowTo.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import cvImg from "@/public/images/cv.png";
+
+import "./HowTo.css";
+
+const HowTo = () => {
+  return (
+    <section className="section-wide how-to">
+      <ul>
+        <h2>Cum creezi un CV?</h2>
+        <li>
+          <Link href="/">
+            <span>1</span> Alege un model
+          </Link>
+        </li>
+
+        <li>
+          <Link href="/">
+            <span>2</span> Creeaza un cont (optional)
+          </Link>
+        </li>
+
+        <li>
+          <Link href="/">
+            <span>3</span> Completeaza datele tale
+          </Link>
+        </li>
+
+        <li>
+          <Link href="/">
+            <span>4</span> Descarca CV-ul si aplica
+          </Link>
+        </li>
+      </ul>
+
+      <div className="img-box">
+        <Image src={cvImg} fill sizes="90vw" alt="cv" />
+      </div>
+    </section>
+  );
+};
+
+export default HowTo;
